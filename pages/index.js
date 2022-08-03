766049c877f20877c473ce66ccdc361c46060064
user <user@users-MacBook-Pro.local>
2022-08-04 02:59:27 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Featured from "./components/Featured";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Resturant In N.Y</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured />
+    </div>
+  );
+}
